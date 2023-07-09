332fb71ef728bc522d0b12e66fc8d03e2f2cc768
marcelolopes <marcelovrb@gmail.com>
2023-07-09 19:11:35 -0300

    products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,38 @@
 class Api::V1::ProductsController < ApplicationController
   def index
+    products = Product.all
+    render json: products, status: 200
+  end
+
+  def create
+    product = Product.new(
+      name: prod_params[:name],
+      brand: prod_params[:brand],
+      price: prod_params[:price],
+      description: prod_params[:description]
+    )
+    if product.save
+      render json:product, status: 200
+    else
+      render json:{error: "Error creating review."}
+    end
   end
 
   def show
+    product = Product.find_by(id: params[:id])
+    if product
+      render json:product, status:200
+    else
+      render json:{error: "Product Not Found."}
   end
+
+  private
+    def prod_params
+      params.require(:product).permit([
+        :name,
+        :brand,
+        :price,
+        :description
+      ])
+    end
 end
